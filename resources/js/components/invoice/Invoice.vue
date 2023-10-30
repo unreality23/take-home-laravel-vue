67c0d072dea5938e55922ff39fc35a09a852c814
vaidotaslipskas@gmail.com <vaidotaslipskas@gmail.com>
2023-10-30 14:00:31 +0000

    added an option to download invoice as pdf

--- resources/js/components/invoice/Invoice.vue
+++ resources/js/components/invoice/Invoice.vue
@@ -1,33 +1,37 @@
 <template>
     <div class="invoice-box">
         <div v-if="invoice">
-            <h3>Invoice Details</h3>
-            <p>Invoice Number: <b>{{ invoice.invoice.invoice_number }}</b></p>
-            <p>Invoice Date: {{ invoice.invoice.invoice_date }}</p>
+            <div class="invoice-data px-3 py-5">
+                <h3>Invoice Details</h3>
+                <p>Invoice Number: <b>{{ invoice.invoice.invoice_number }}</b></p>
+                <p>Invoice Date: {{ invoice.invoice.invoice_date }}</p>
 
-            <h3>Products List</h3>
-            <table>
-                <thead>
+                <h3>Products List</h3>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="detail in orderDetails" :key="detail.id">
+                        <td>{{ detail.product.name }}</td>
+                        <td>{{ detail.quantity }}</td>
+                        <td>{{ detail.price }}£</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <tfoot>
                 <tr>
-                    <th>Product Name</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
+                    <td colspan="2" style="text-align: right;">Total Amount:</td>
+                    <td>{{ invoice.invoice.total_amount }}£</td>
                 </tr>
-                </thead>
-                <tbody>
-                <tr v-for="detail in orderDetails" :key="detail.id">
-                    <td>{{ detail.product.name }}</td>
-                    <td>{{ detail.quantity }}</td>
-                    <td>{{ detail.price }}£</td>
-                </tr>
-                </tbody>
-            </table>
-            <tfoot>
-            <tr>
-                <td colspan="2" style="text-align: right;">Total Amount:</td>
-                <td>{{ invoice.invoice.total_amount }}£</td>
-            </tr>
-            </tfoot>
+                </tfoot>
+            </div>
+
+            <button @click="downloadInvoice">Download Your Invoice in PDF</button>
         </div>
         <div v-else>
             <p>No invoice available for this order</p>
@@ -37,6 +41,8 @@
 </template>
 
 <script>
+import html2pdf from "html2pdf.js";
+
 export default {
     name: "Invoice",
     props: ['orderId'],
@@ -45,6 +51,12 @@ export default {
             invoice: null,
             orderDetails: []
         };
+    },
+    methods: {
+      downloadInvoice() {
+          let invoiceContent = document.querySelector('.invoice-data');
+          html2pdf(invoiceContent);
+      }
     },
     mounted() {
         axios.get(`/api/orders/${this.orderId}/invoice`)
